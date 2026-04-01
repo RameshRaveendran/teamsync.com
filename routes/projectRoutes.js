562bf21e6a30291f7b51e7d7f0b8f9e62498f2ca
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-04-01 12:09:28 +0530

    project route

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  createProject,
-  getProjects,
-  addMember
-} = require("../controllers/projectController");
+const projectController = require("../controllers/projectController");
+const { protect } = require("../middleware/authMiddleware");
 
-const protect = require("../middleware/authMiddleware");
+// DEBUG (VERY IMPORTANT)
+console.log("createProject:", typeof projectController.createProject);
+console.log("getProjects:", typeof projectController.getProjects);
+console.log("addMember:", typeof projectController.addMember);
+console.log("protect:", typeof protect);
 
-router.post("/", protect, createProject);
-router.get("/", protect, getProjects);
-router.put("/:id/add-member", protect, addMember);
+router.post("/", protect, projectController.createProject);
+router.get("/", protect, projectController.getProjects);
+router.put("/:id/add-member", protect, projectController.addMember);
 
 module.exports = router;
