a4b8aed3c55753c047a472e2c48331894d29da46
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-04-01 12:05:31 +0530

    task route debug mode

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const {
-  createTask,
-  getTasks,
-  updateTaskStatus
-} = require("../controllers/taskController");
 
+const taskController = require("../controllers/taskController");
 const { protect } = require("../middleware/authMiddleware");
 
-router.post("/", protect, createTask);
-router.get("/:projectId", protect, getTasks);
-router.put("/:id", protect, updateTaskStatus);
+// DEBUG CHECK (REMOVE LATER)
+console.log("createTask:", typeof taskController.createTask);
+console.log("protect:", typeof protect);
+
+router.post("/", protect, taskController.createTask);
+router.get("/:projectId", protect, taskController.getTasks);
+router.put("/:id", protect, taskController.updateTaskStatus);
 
 module.exports = router;
