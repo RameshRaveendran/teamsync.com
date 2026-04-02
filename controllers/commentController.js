c12cfdf107e9806d90588bb2d5ed8950cf26fbb2
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-04-02 11:02:22 +0530

    comment controller check tasker

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,3 +1,26 @@
 const Comment = require("../models/Comment");
 const Task = require("../models/Task");
 const Project = require("../models/Project");
+
+const addComment = async (req, res) => {
+  const { taskId, text } = req.body;
+
+  // 1. Check task exists
+  const task = await Task.findById(taskId);
+  if (!task) return res.status(404).json({ message: "Task not found" });
+
+  // 2. Check user is part of project
+  const project = await Project.findById(task.projectId);
+  if (!project.members.includes(req.user.id)) {
+    return res.status(403).json({ message: "Not allowed" });
+  }
+
+  // 3. Create comment
+  const comment = await Comment.create({
+    taskId,
+    userId: req.user.id,
+    text
+  });
+
+  res.json(comment);
+};
