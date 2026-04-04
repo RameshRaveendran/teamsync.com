1bd06ad9d8bd9156ec2dbbfba12a8a71cd708b27
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-04-04 15:32:07 +0530

    user token updated

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,9 +1,14 @@
 const jwt = require("jsonwebtoken");
 
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "7d"
-  });
+const generateToken = (user) => {
+  return jwt.sign(
+    {
+      id: user._id,
+      role: user.role   // 🔥 IMPORTANT
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
 };
 
 module.exports = generateToken;
