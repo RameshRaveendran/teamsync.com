8b5ae9ea46520cb9a8029667e5835a3e7e713a8b
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-04-04 15:46:02 +0530

    error updated

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const protect = require("../middleware/authMiddleware");
-const authorizeRoles = require("../middleware/roleMiddleware");
+const {protect} = require("../middleware/authMiddleware");
+const {authorizeRoles} = require("../middleware/roleMiddleware");
 
 const { createTask } = require("../controllers/taskController");
 
